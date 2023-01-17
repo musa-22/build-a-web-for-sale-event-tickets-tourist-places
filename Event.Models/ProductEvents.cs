e498d6593e28e106ef7ae1542a324588ab101ea7
musa-22 <musasaleih@gmail.com>
2023-01-17 13:42:17 +0000

    add models and update error

---
+++ Event.Models/ProductEvents.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Eevent.Models
+{
+    
+        /*
+         * here product event modeling created. for instance selling tickets for events .... 
+         */
+    public class ProductEvents
+    {
+
+        
+        public int Id { get; set; }
+
+        // event title 
+        [Required]
+        public string Tile { get; set; }
+
+
+        // short description 
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double listPrice { get; set; }
+
+        // final price after discount
+        [Required]
+        public double price { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+
+        // create foreign key between  models 
+
+        public int CategortEeventId { get; set; }
+        [Required]
+        [ForeignKey("CategortEeventId")]
+        public CategoryEevent CategoryEevent { get; set; }
+
+        public int EventsTypesId { get; set; }
+        [Required]
+        public EventsTypes EventsTypes { get; set; }
+
+    }
+}
